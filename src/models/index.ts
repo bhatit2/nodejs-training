4845a04a16a714e56d2f0caafe87482a81105585
Tanvi Bhatia <tanvi_bhatia@epam.com>
2020-10-04 22:53:28 +0530

    removed .env tracking

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,12 @@ import { Sequelize } from "sequelize";
 import userModel from './user';
 import groupModel from './group';
 import userGroupModel from './userGroup';
+const dotenv = require("dotenv");
 
-const sequelize = new Sequelize(
-  "postgres://asdrdbbr:az0-Ot6N-fILlzBQxgTUoO5Vygv_VliD@raja.db.elephantsql.com:5432/asdrdbbr"
-);
+// get config vars
+dotenv.config();
+
+const sequelize = new Sequelize(process.env.POSTGRESQL as string);
 
 const Models = {
   User : userModel(sequelize),
